dda3844ee7b18d5645f8cd042f2095a0824bfa4e
jgonz01 <jgonz117@ucr.edu>
2016-11-01 22:15:29 -0700

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include "Command.h"
+#include <string.h> 
+using namespace std;
 
+int main(){
+    
+    Command* test = new Command();
+    
+    //LOOP TO CHECK FOR EXIT
+    while(test->exitflag != true){
+        
+        test = new Command();
+        test->getInput();
+        
+        if(test->checkEnter() != true){
+            test->callConnectors();
+        }
+    }
+    
+    return 0;
+}
